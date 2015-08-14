5f2ce351ff115f491733db5ecf2e01fb1ae5358c
Priyanshu Sekhar <pri.patra@gmail.com>
2015-08-14 07:13:04 +0530

    Updated thrift libraries

--- psptest/output/html5/util/ws_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /opt/lampp/htdocs/psptest/util
-
-mkdir ../log 2> /dev/null
-
-nohup php msg-wsserver.php 2>&1 > ../log/ws.out &
-nohup php msg-udpserver.php 2>&1 > ../log/udp.out &
